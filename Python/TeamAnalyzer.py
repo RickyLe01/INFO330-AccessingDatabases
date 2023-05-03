438b4634c7af309a44456f434ec79cfc161bddc7
RickyLe01 <RickyDangLe01@gmail.com>
2023-05-02 21:37:05 -0700

    Version 2

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -22,6 +22,29 @@ for i, arg in enumerate(sys.argv):
     # Remember to look at those "against_NNN" column values; greater than 1
     # means the Pokemon is strong against that type, and less than 1 means
     # the Pokemon is weak against that type
+    cursor = con.cursor()
+    if (arg.isnumeric()):
+        name_types = ("SELECT p.name, pv.type1, pv.type2 FROM pokemon p JOIN pokemon_types_view pv ON p.name = pv.name WHERE p.id = " + arg + ";")
+        cursor.execute(name_types)
+    elif (arg is not None):
+        name_types = ('SELECT p.name, pv.type1, pv.type2 FROM pokemon p JOIN pokemon_types_view pv ON p.name = pv.name WHERE p.name = "' + arg + '";')
+        cursor.execute(name_types)
+    result = cursor.fetchone()
+
+
+    for type in types:
+        sql = ("SELECT pv.against_" + type + " " +
+            "FROM pokemon_types_battle_view pv " +
+            "WHERE type1name = '" + result[1] + "' AND type2name = '" + result[2] +"';")
+        cursor.execute(sql)
+        test = cursor.fetchone()
+        if (test[0] > 1.0):
+            weakness.append(type)
+        elif (test[0] < 1.0):
+            strong.append(type)
+    print(result[0] + " (" + result[1] + " " + result[2] + ") " + "is strong against", strong ,"but is weak against", weakness)
+con.close()
+
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
